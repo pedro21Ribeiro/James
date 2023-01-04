7200608cc513182cacf976b049f98794c3a28ace
pedro21Ribeiro <71394807+pedro21Ribeiro@users.noreply.github.com>
2023-01-04 11:31:41 -0300

    Update james.py
    cleaned functions and code added comments for reading quality

--- james.py
+++ james.py
@@ -1,9 +1,11 @@
 #this is just a program not for realy coding but just for chilling. Enjoy (=
+#importing repositories
+
 import time as t
 import requests as req
 import random as r
 
-
+#initialization of the program
 def james():
     print("Hi.")
     print(".")
@@ -15,30 +17,24 @@ def james():
     print("Welcome to James. A chill program.")
     t.sleep(2)
     menu()
+
+#main menu function can scale
 def menu():
-    ui = input("What do you want to do today?\n1- Daily motivation 2- Harsh but true 3- exit"+ "\n")
+    ui = input("What do you want to do today?\n1- Daily motivation 2- exit"+ "\n")
     if ui == "1":
         Dm()
     elif ui == "2":
-        Hbt()
-    elif ui == "3":
         print("Good bye")
     else:
         print("This is not a vallid option")
         menu()
+#daily motivation functionc uses a api to get a quote then cleans the response gettin only the quote
 def Dm():
     t.sleep(2)
     result = req.get("https://zenquotes.io/api/random")
     clean_result = result.json()
     print(clean_result[0].get("q")+ "\n")
     t.sleep(2)
-    menu()
-def Hbt():
-    hbtFile = open("hbt.txt")
-    readed = hbtFile.read()
-    splited = readed.split("\n")
-    num = r.randrange(0, len(splited) - 1)
-    print(splited[num]+"\n")
     menu()
 
 
